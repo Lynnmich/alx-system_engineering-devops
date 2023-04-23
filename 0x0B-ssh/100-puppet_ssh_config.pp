3f8c328e54af73cf2cb6a0417cdd20eb6e6fc687
Lynnmich <lynnenmichuki@gmail.com>
2023-04-23 07:16:03 -0700

    Config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Client configuration file (w/ Puppet)
+
+file_line {' Turn off passwd auth':
+  path => 'etc/ssh/sshd_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path => 'etc/ssh/sshd_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
