15d5e2ee3d084faeaf57cfd72408009c31bef891
Lynne Michuki <lynnenmichuki@gmail.com>
2023-06-06 18:41:43 +0300

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and
+returns the number of subscribers for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that returns number of subsribers of a subreddit passed """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = "Mozilla/5.0"
+
+    response = requests.get(url, headers=user_agent)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
