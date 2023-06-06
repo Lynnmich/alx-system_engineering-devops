c309ae9a27276bfae7c7e3bc1247b04f1f5c0a24
Lynne Michuki <lynnenmichuki@gmail.com>
2023-06-06 20:57:13 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers
-for a given subreddit.
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
 """
 import requests
 
@@ -13,9 +12,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-         AppleWebKit/537.36 (KHTML, like Gecko)
-         Chrome/58.0.3029.110 Safari/537.3"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
